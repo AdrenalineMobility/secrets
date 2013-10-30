c64b0cb78eb8c50eee03b1219c6952af6a3a55aa
murph <murph@murph.cc>
2013-10-30 10:00:07 -0700

    Remove actionbar code from Splash, we don't use the AB there

--- Secrets/src/main/java/io/adrenaline/secrets/Splash.java
+++ Secrets/src/main/java/io/adrenaline/secrets/Splash.java
@@ -39,16 +39,4 @@ public class Splash extends ActionBarActivity {
         getMenuInflater().inflate(R.menu.splash, menu);
         return true;
     }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        switch (item.getItemId()) {
-            case R.id.action_settings:
-                return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
 }
