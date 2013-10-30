afc9d79254f9feed58d98fcc77434d95b7a80330
murph <murph@murph.cc>
2013-10-30 10:41:00 -0700

    Code cleanup; Remove action bar code from splash

--- Secrets/src/main/java/io/adrenaline/secrets/Splash.java
+++ Secrets/src/main/java/io/adrenaline/secrets/Splash.java
@@ -11,7 +11,7 @@ import android.widget.TextView;
 
 import io.adrenaline.AdrenalineIo;
 
-public class Splash extends ActionBarActivity {
+public class Splash extends Activity {
     private static final String TAG = "AdrenalineSecrets";
 
     @Override
@@ -22,19 +22,11 @@ public class Splash extends ActionBarActivity {
         AdrenalineIo.init(getApplicationContext());
 
         final TextView button = (TextView) findViewById(R.id.login_button);
+
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 startActivity(new Intent(Splash.this, GroupListActivity.class));
             }
         });
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.splash, menu);
-        return true;
     }
 }
